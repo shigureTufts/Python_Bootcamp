525f58b1090cf496dde614b868b111f4ddc45bc0
ShigureTufts <ju-hung.chen@tufts.edu>
2024-05-20 19:15:26 -0400

    Add user input check for RPS game

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,11 +2,19 @@ print("...Rock...")
 print("...Paper...")
 print("...Scissors...\n")
 
+# Take inputs and check if users input the right thing
 print("Enter player 1's choice: ")
 player_1 = input()
+if player_1 != "Rock" or player_1 != "Paper" or player_1 != "Scisors":
+    print("Input Error!")
+    quit()
 print("Enter player 2's choice: ")
 player_2 = input()
 
+# Check who is the winner
+if player_2 != "Rock" or player_2 != "Paper" or player_2 != "Scisors":
+    print("Input Error!")
+    quit()
 if player_1 == player_2:
     print("SHOOT!")
     print("It is a tie!")
