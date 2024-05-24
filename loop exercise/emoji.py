1d1c88d5203212b2a7ad4f1439b9c10f24476891
shigureTufts <123694861+shigureTufts@users.noreply.github.com>
2024-05-23 22:09:43 -0400

    add new while loop exercise emoji.py

--- loop exercise/emoji.py
+++ loop exercise/emoji.py
@@ -6,3 +6,9 @@ while counter <= 10:
 print()
 for integer in range(1, 11):
     print("\U0001f600" * integer)
+
+print()
+counter = 1
+while counter <= 20:
+    print("  " * int((20-counter)/2), "\U0001f600" * counter, " " * int((20-counter)/2))
+    counter += 2
