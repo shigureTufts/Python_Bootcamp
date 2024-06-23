4bb287d8f4e3315cb3c7be10c5254da81ff3d83a
shigureTufts <123694861+shigureTufts@users.noreply.github.com>
2024-06-22 22:04:37 -0400

    update ascii_art.py

--- module/ascii_art.py
+++ module/ascii_art.py
@@ -1,7 +1,13 @@
 import pyfiglet
 from termcolor import colored
+color_list = ("red", "green", "yellow", "blue", "magenta", "cyan", "white")
+
 
 message = pyfiglet.figlet_format(input("What message do you want to print? "))
-color = input("What color? ")
+color = input("What color? ").lower()
 
-print(colored(message, color=color))
+if color in color_list:
+    print(colored(message, color=color))
+else:
+    print(colored(message, color="magenta"))
+
