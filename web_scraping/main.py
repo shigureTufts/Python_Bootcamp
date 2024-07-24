f7756a3cffa95309e050a42d89a664899ad3dd27
shigureTufts <123694861+shigureTufts@users.noreply.github.com>
2024-07-24 13:52:42 -0400

    update scraping project

--- web_scraping/main.py
+++ web_scraping/main.py
@@ -1,5 +1,5 @@
 import requests
-import bs4
+from bs4 import BeautifulSoup
 
 url = "http://quotes.toscrape.com/"
 # Requesting HTML from server
@@ -7,6 +7,15 @@ content = requests.get(url)
 # Check text is HTML and check tags
 # print(content.text)
 
-# Soup time
-soup = bs4.BeautifulSoup(content.text, 'html.parser')
-print(soup.body.span)
+# get quotes from page 1
+all_quote = []
+soup = BeautifulSoup(content.text, 'html.parser')
+quotes = soup.find_all(class_="quote")
+for quote in quotes:
+    all_quote.append({
+        "text": quote.find(class_='text').get_text(),
+        "author": quote.find(class_='author').get_text(),
+        "bio-link": quote.find("a")["href"]
+    })
+
+print(all_quote)
